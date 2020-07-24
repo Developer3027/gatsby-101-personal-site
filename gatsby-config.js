7514d2c151b2c6278ef47613780cfb961f0622ad
mason roberts <mrobe18@gmail.com>
2020-07-24 18:32:59 -0400

    blog route

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ['gatsby-plugin-mdx']
+  plugins: [
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-theme-blog',
+      options: {
+        basePath: '/blog',
+        mdxOtherwiseConfigured: true
+      }
+    }
+  ]
 }
