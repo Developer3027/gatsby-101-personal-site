3b571fcf4c81e3462961271b947e59b8cf8d4c09
mason roberts <mrobe18@gmail.com>
2020-07-24 18:02:31 -0400

    added plugin and about page

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('/home/masonr/working/gatsby-101-personal-site/node_modules/gatsby-plugin-mdx/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/home/masonr/working/gatsby-101-personal-site/.cache/dev-404-page.js"))),
+  "component---src-pages-about-mdx": hot(preferDefault(require("/home/masonr/working/gatsby-101-personal-site/src/pages/about.mdx"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/masonr/working/gatsby-101-personal-site/src/pages/index.js")))
 }
 
