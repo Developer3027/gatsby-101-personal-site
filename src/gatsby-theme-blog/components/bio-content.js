345bb3b0e8d921a680b9bc17c5961406633a2bd1
mason roberts <mrobe18@gmail.com>
2020-07-24 18:51:39 -0400

    shadow content

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Styled } from 'theme-ui'
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+const BioContent = () => (
+  <>
+    Words by <Styled.a href='https://mason-roberts.com'>Mason Roberts</Styled.a>
+    .
+    <br />
+    Born bright, Life has dulled me.
+  </>
+)
+
+export default BioContent
