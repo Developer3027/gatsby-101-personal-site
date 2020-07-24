8bb74384ebfaf2cee17b94032802c83f129ee9ac
mason roberts <mrobe18@gmail.com>
2020-07-24 17:52:23 -0400

    add p to main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Home () {
-  return <div>Hello, Codeland!</div>
+  return (
+    <div>
+      Hello, Codeland!
+      <p>what?</p>
+    </div>
+  )
 }
 
 export default Home
